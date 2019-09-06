00d2d3d4eed48979dafae69eb88eb2d04feb8318
hyx <754060604@qq.com>
2019-09-06 14:37:40 +0800

    更新verification方法的统一的错误消息定制

--- src/main.js
+++ src/main.js
@@ -1,81 +1,84 @@
 import Validator from './Validator/Validator';
 
 class StaticValidator {
-    constructor() {
-        this.errors = [];
-        this.validator = null;
-    }
+	constructor() {
+		this.errors = [];
+		this.validator = null;
+	}
 
-    /**
-     *
-     * @param rules             {string}
-     * @param customMessages    {Array}
-     * @param customNames       {Object}
-     * @param value             {string}
-     * @param callBack          {function}
-     * @return {*}
-     */
-    verification({rules, customMessages = {}, customNames}, value, callBack) {
-        if (typeof rules !== 'string' && Array.isArray(rules) === false) {
-            return callBack();
-        }
-        let data = {
-            name: value
-        };
-        rules = {
-            name: rules
-        };
+	/**
+	 *
+	 * @param rules             {String}
+	 * @param customMessages    {Array}
+	 * @param customNames       {Object}
+	 * @param messages          {String}
+	 * @param value             {String}
+	 * @param callBack          {function}
+	 * @return {*}
+	 */
+	verification({rules, customMessages = {}, customNames, messages}, value, callBack) {
+		if (typeof rules !== 'string' && Array.isArray(rules) === false) {
+			return callBack();
+		}
+		let data = {
+			name: value
+		};
+		rules = {
+			name: rules
+		};
 
-        this.validator = Validator.make(data, rules, customMessages, customNames);
+		this.validator = Validator.make(data, rules, customMessages, customNames);
 
-        if (this.validator.fails()) {
-            this.errors = this.validator.getError('name');
-            if (this.errors  && this.errors .length > 0) {
-                return callBack(new Error(this.errors[0]));
-            }
-        }
-        return callBack();
-    }
+		if (this.validator.fails()) {
+			this.errors = this.validator.getError('name');
+			if (messages !== undefined) {
+				callBack(new Error(messages));
+			} else if (this.errors && this.errors.length > 0) {
+				return callBack(new Error(this.errors[0]));
+			}
+		}
+		return callBack();
+	}
 
-    /**
-     *
-     * @param rules             {string | Array}
-     * @param value             {string}
-     * @param customMessages    {Array}
-     * @param customNames       {Object}
-     * @returns {Error|boolean}
-     */
-    valid(rules, value, customMessages = [], customNames) {
-        let data = {
-            name: value
-        };
-        let dataRules = {
-            name: rules
-        };
+	/**
+	 *
+	 * @param rules             {string | Array}
+	 * @param value             {string}
+	 * @param customMessages    {Array}
+	 * @param customNames       {Object}
+	 * @returns {Error|boolean}
+	 */
+	valid(rules, value, customMessages = [], customNames) {
+		let data = {
+			name: value
+		};
+		let dataRules = {
+			name: rules
+		};
 
-        this.validator = Validator.make(data, dataRules, customMessages, customNames);
+		this.validator = Validator.make(data, dataRules, customMessages, customNames);
 
-        if (this.validator.fails()) {
-            this.errors  = this.validator.getError('name');
-            if (this.errors  && this.errors .length > 0) {
-                return new Error(this.errors[0]);
-            }
-        } else {
-            return true;
-        }
-    }
+		if (this.validator.fails()) {
+			this.errors = this.validator.getError('name');
+			if (this.errors && this.errors.length > 0) {
+				return new Error(this.errors[0]);
+			}
+		} else {
+			return true;
+		}
+	}
 
-    /**
-     *
-     * @param data              {Object}
-     * @param dataRules         {Object}
-     * @param customMessages    {Array}
-     * @param customNames       {Object}
-     * @returns {Validator}
-     */
-    make(data, dataRules, customMessages = [], customNames) {
-        return Validator.make(data, dataRules, customMessages, customNames);
-    }
+	/**
+	 *
+	 * @param data              {Object}
+	 * @param dataRules         {Object}
+	 * @param customMessages    {Array}
+	 * @param customNames       {Object}
+	 * @returns {Validator}
+	 */
+	make(data, dataRules, customMessages = [], customNames) {
+		return Validator.make(data, dataRules, customMessages, customNames);
+	}
 }
 
 export default new StaticValidator();
--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 const Validator = require('../dist/index');
 
-console.info(Validator);
+let data = {
+	rules: [],
 
-let result = Validator.valid('required|max:10|string|min:5', 'abc', {string: '12344'});
-
-console.info(Validator.errors);
+};
