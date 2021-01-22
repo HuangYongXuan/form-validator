89e93a3ba1052238730d98e6c34a126fad2ee671
HuangYongXuan <754060604@qq.com>
2021-01-22 15:17:57 +0800

    修复ip严重规则

--- test/test.js
+++ test/test.js
@@ -1,17 +1,21 @@
 const Validator = require('../dist/index');
 
-let data = {
-    name: 9.12
-};
+function testValidatorIp() {
+    let data = {
+        name: '10.233.12.12'
+    };
 
-let rules = {
-    name: ['required', 'numeric', 'min:0', 'max:10', 'decimal:2']
-};
+    let rules = {
+        name: ['required', 'ip']
+    };
 
-let v = Validator.make(data, rules, {}, {});
+    let v = Validator.make(data, rules, {}, {});
 
-if (v.fails()) {
-    console.error(v.getErrors())
-} else {
-    console.info('success')
+    if (v.fails()) {
+        console.error(v.getErrors())
+    } else {
+        console.info('success')
+    }
 }
+
+testValidatorIp();
